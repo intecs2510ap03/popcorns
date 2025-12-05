ac791b6ecfca9c761f7fd17183cfa019ab95cffb
10 <intecs.2510ap.10@gmail.com>
2025-12-05 15:39:27 +0900

    12月５日帰る前のコミット：戻るボタン作成完了

--- answer.php
+++ answer.php
@@ -1,6 +1,46 @@
 <?php require 'header.php'; ?>
 <?php require 'function.php';?>
 
-<?php echo 'smile'; ?>
+<?php 
+
+/* 
+echo '<input type="submit" value="ログアウト">'
+
+if(isset($_SESSION['user'])){
+    unset($_SESSION['user']);
+    header('Location:index.php');
+    exit;
+}
+*/
+
+/*
+・アンセット、でログアウトする機能を作る→インデックスに戻る
+
+・質問内容を引っ張ってくる
+→質問IDを質問内容を引っ張ってくる（
+
+function addAnswer($['id],questionID,userID,answer){
+
+}return ;
+
+$row['question']/* 質問内容の関数 
+$row['questionID']
+
+$row['answer']['id'] /* 回答のID 
+$row['answer'] /* 回答の内容 */
+/*
+・回答欄のフォームをつくる
+・input type="text"で入力欄
+・登録はsubmit value="登録"
+・使用する関数＝addAnswer（DBに回答を登録）
+
+ */
+
+
+?>
+
+<!-- 戻るボタン -->
+<form action="detail.php" method="get">
+<input type="submit" value="戻る">
 
 <?php require 'footer.php'; ?>
