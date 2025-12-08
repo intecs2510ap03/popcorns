6dfd715cf55c616835b93414e424b289161b1e4f
user15 <intecs.2510ap.15@gmail.com>
2025-12-08 15:54:28 +0900

    addQuestion追加

--- function.php
+++ function.php
@@ -12,5 +12,17 @@
         $sql->execute([$userid, $userpw]);
         return $sql;
     }
-	
+	//addQuestion
+    //書き方　addQuestion($_SESSION['user']['id'], $_POST['question'])
+    function addQuestion($userId, $question){
+    $pdo = Connect();
+    $sql = $pdo->prepare('INSERT INTO question VALUES (null, ?, ?, now(), 0)');
+    return $sql->execute([$userId,$question]);
+    }
+
+    // function addQuestion($questionId,$userId, $answer){
+    // $pdo = Connect();
+    // $sql = $pdo->prepare('INSERT INTO answer VALUES (null, ?, ?, ?, now(), 0)');
+    // return $sql->execute([$questionId,$userId,$answer]);
+    // }
 ?>
