631079bd8d85efcab4130ab72b53978bdab3b15c
user03 <intecs.2510ap.03@gmail.com>
2025-12-08 16:46:45 +0900

    Function関数に追記 from detail.php

--- function.php
+++ function.php
@@ -5,12 +5,29 @@
         return new PDO('mysql:host=localhost;dbname=ideastock;charset=utf8', 'staff', 'password');
     }
 
-    // isUser関数
-    // 書き方　isUser($userid, $userpw, $pdo)
-    function isUser($userid, $userpw, $pdo){
+    // getUser関数
+    // 書き方　getUser($userid, $userpw, $pdo);
+    function getUser($userid, $userpw, $pdo){
         $sql = $pdo->prepare('select * from user where loginId=? and password=?');
         $sql->execute([$userid, $userpw]);
         return $sql;
+    }
+
+    // getQuestion関数
+    // 書き方　getQuestion($pdo);
+    function getQuestion($pdo){
+        $sql = $pdo->query('select * from question');
+        $user_All = $sql->fetchAll();
+        return $user_All;
+    }
+
+    // getUserName関数
+    // 書き方　getUserName($id,$pdo);
+    function getUserName($id,$pdo){
+        $sql = $pdo->prepare('select name from user where id=?');
+        $sql->execute([$id]);
+        $name = $sql->fetchColumn(); 
+        return $name;
     }
 	
 ?>
