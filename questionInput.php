432b58c2a756024415602cd64a3f66da2b7bde8d
user15 <intecs.2510ap.15@gmail.com>
2025-12-08 15:48:54 +0900

    8割り終わりました

--- questionInput.php
+++ questionInput.php
@@ -1,23 +1,29 @@
 <?php require 'header.php'; ?>
 <?php require 'function.php';?>
 <?php
+session_start();
 echo '<th>質問を投稿する</th>';
 echo '<br>';
 echo '<form acion="questionInput.php" method="post">';
-echo '<input type="textarea" name="question" cols="40" rows="4" value= "">';
+echo '<textarea name="question" cols="70" rows="4">';
+echo '</textarea>';
 echo '<br>';
 echo '<input type="submit" value="登録">';
 
-if(isset($_SESSION['user'])) {
-    $pdo = Connect();
-    $sql=$pdo->prepare('insert into question values (null,?,?,now(),0)');
-    $sql->execute([
-        $_REQUEST['userId'],$_REQUEST['question'],$_REQUEST['date']
-    ]);
-}else{
-    echo '質問を投稿できませんでした。';
+if (isset($_POST['question'])) {
+    $question = $_POST['question'];
+    if (trim($question)==="") {
+        echo  '質問を入力してください';
+    } elseif (mb_strlen($question) > ) {
+        echo '質問は250文字以内で入力してください';
+    } elseif (addQuestion($_SESSION['user']['id'], $question)) {
+        header('Location: questions.php');
+        exit;
+    }
 }
 echo '<br>';
+echo '<form action="questions.php" method="GET">';
 echo '<input type="submit" value="戻る">';
+echo '</form>';
 ?>
 <?php require 'footer.php'; ?>
