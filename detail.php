3b51ce5e113b9d57493938fcd0496e0f9a2cb407
user03 <intecs.2510ap.03@gmail.com>
2025-12-08 16:45:34 +0900

    Detail.phpの追記

--- detail.php
+++ detail.php
@@ -6,50 +6,46 @@
     // PDO関数
     $pdo = Connect();
 
-    echo '<p>質問</p>';
-
     // 質問を全部取得
     $question_all = getQuestion($pdo);
     
+
+    echo '<p>質問</p>';
     echo '<p>';
+    $questionId = $_REQUEST['questionId'] -1;
     // 質問内容
-    $name = getUserName($question_all[$_REQUEST['questionId']]['userId'],$pdo);
+    $name = getUserName($question_all[$questionId]['userId'],$pdo);
     echo $name, '<br>';
-    echo $question_all[$_REQUEST['questionId']]['id'], '<br>';
-    echo $question_all[$_REQUEST['questionId']]['question'], '<br>';
+    echo $question_all[$questionId]['question'], '<br>';
+    echo $question_all[$questionId]['date'], '<br>';
 
     // 回答ボタン
     echo '<form action="answer.php" method="post">';
-    echo '<input type="hidden" name="questionId" value="', $row['id'] ,'">';
-    echo '<input type="submit" value="詳細">';
+    echo '<input type="hidden" name="questionId" value="', $questionId ,'">';
+    echo '<input type="submit" value="回答">';
     echo '</form>';
     echo '</p>';
 
     // 回答内容
+    echo '<p class="answer-box">回答</p>';
 
-
-    // foreach ($question_all as $row) {
-    //     echo '<p>';
-    //     // ユーザの名前を取得する
-    //     $name = getUserName($row['userId'],$pdo);
-    //     echo $name, '<br>';
-    //     echo $row['question'], '<br>';
-    //     echo $row['date'];
-    //     echo '<form action="detail.php" method="post">';
-    //     echo '<input type="hidden" name="questionId" value="', $row['id'] ,'">';
-    //     echo '<input type="submit" value="詳細">';
-    //     echo '</form>';
-    //     echo '</p>';
-    // }
-
-
-
-    // echo $user_name;
-    // echo '<br>';
-    // echo $question_question;
-    // echo '<br>';
-    // echo $question_date;
-    // echo '<br>';
+    $sql = $pdo->prepare('select * from answer where questionId=?');
+    $sql->execute([$_REQUEST['questionId']]);
+    $answer_All = $sql->fetchAll();
+    
+    foreach ($answer_All as $row) {
+        echo '<p class="answer-box">';
+        // ユーザの名前を取得する
+        $name = getUserName($row['userId'],$pdo);
+        echo $name, '<br>';
+        echo $row['answer'], '<br>';
+        echo $row['date'];
+        echo '<form action="detail.php" method="post">';
+        echo '<input type="hidden" name="questionId" value="', $row['id'] ,'">';
+        echo '<input type="submit" value="詳細">';
+        echo '</form>';
+        echo '</p>';
+    }
 
 ?>
 
