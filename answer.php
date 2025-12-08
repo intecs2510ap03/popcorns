9d4c85b6c29b69c23bf84e913b933e4c1827ac38
10 <intecs.2510ap.10@gmail.com>
2025-12-08 15:45:06 +0900

    ８日ぶんコミットする

--- answer.php
+++ answer.php
@@ -1,41 +1,77 @@
+<?php session_start(); ?>
 <?php require 'header.php'; ?>
 <?php require 'function.php';?>
 
-<?php 
+<?php
 
-/* 
-echo '<input type="submit" value="ログアウト">'
+$pdo = Connect();
 
-if(isset($_SESSION['user'])){
-    unset($_SESSION['user']);
-    header('Location:index.php');
-    exit;
+
+    echo '<p>質問</p>';
+
+    // 名前取得
+    $sql=$pdo->prepare('select * from user where id=?');
+    $sql->execute([1]);
+    foreach($sql as $row) {
+        $user_id = $row['id'];
+        $user_loginId = $row['loginId'];
+        $user_password = $row['password'];
+        $user_name = $row['name'];
+    }
+
+    // 質問内容取得
+    $sql=$pdo->prepare('select * from question where userId=?');
+    $sql->execute([$user_id]);
+    foreach($sql as $row) {
+        $question_id = $row['id'];
+        $question_userId = $row['userId'];
+        $question_question = $row['question'];
+        $question_date = $row['date'];
+        $question_delFlg = $row['deleteFlg'];
+    }
+
+    echo $user_name;
+    echo '<br>';
+    echo $question_question;
+    echo '<br>';
+    echo $question_date;
+    echo '<br>';
+
+
+
+echo '回答';
+
+echo '<form action="answer.php" method="POST">';
+echo '<input type="text" name="answer">';
+echo '<input type="hidden" name="question_id" value="1">';
+echo '<input type="submit" value="登録">';
+
+
+if (isset($_POST['answer'])) {
+    $answer = $_POST['answer'];
+    $questionId = (int)$_POST['question_id']; // 空白除去
+    if (trim($answer) === "") {
+        echo '回答を入力してください';
+    } elseif (mb_strlen($answer) > 250) {
+        echo '回答は250文字以内で入力してください';
+    } elseif (addAnswer($_SESSION['user']['id'], $question_id, $answer)) {
+        header('Location: questions.php');
+        exit;
+    } else {
+        echo '回答の登録に失敗しました';
+    }
 }
-*/
 
-/*
-・アンセット、でログアウトする機能を作る→インデックスに戻る
+           // values(null,$question_id,$question_userId,$answer,date,dateFIg)' ); 
+            //id,questionId,userId,answer,date,dateFlg
 
-・質問内容を引っ張ってくる
-→質問IDを質問内容を引っ張ってくる（
-
-function addAnswer($['id],questionID,userID,answer){
-
-}return ;
-
-$row['question']/* 質問内容の関数 
-$row['questionID']
-
-$row['answer']['id'] /* 回答のID 
-$row['answer'] /* 回答の内容 */
-/*
-・回答欄のフォームをつくる
-・input type="text"で入力欄
-・登録はsubmit value="登録"
-・使用する関数＝addAnswer（DBに回答を登録）
-
- */
-
+//addQuestion
+    /* 書き方　addQuestion($_SESSION['user']['id'], $_POST['question'])
+    function addQuestion($userId, $question){
+    $pdo = Connect();
+    $sql = $pdo->prepare('INSERT INTO question VALUES (null, ?, ?, now(), 0)');
+    return $sql->execute([$userId,$question]);
+    }*/
 
 ?>
 
