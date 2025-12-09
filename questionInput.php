cb67769fb55aac96ab95d55d8ffbdb0e94669f2a
user15 <intecs.2510ap.15@gmail.com>
2025-12-09 11:23:02 +0900

    ほぼ完成

--- questionInput.php
+++ questionInput.php
@@ -4,22 +4,24 @@
 session_start();
 echo '<th>質問を投稿する</th>';
 echo '<br>';
-echo '<form acion="questionInput.php" method="post">';
+echo '<form action="questionInput.php" method="post">';
 echo '<textarea name="question" cols="70" rows="4">';
 echo '</textarea>';
 echo '<br>';
 echo '<input type="submit" value="登録">';
+echo '</form>';
 
 if (isset($_POST['question'])) {
-    $question = $_POST['question'];
+    $question = mb_convert_kana($_POST['question'], "s");
     if (trim($question)==="") {
-        echo  '質問を入力してください';
-    } elseif (mb_strlen($question) > ) {
-        echo '質問は250文字以内で入力してください';
-    } elseif (addQuestion($_SESSION['user']['id'], $question)) {
+        $err = '質問を入力してください';
+    } elseif (mb_strlen($question) > 250) {
+        $err = '質問は250文字以内で入力してください';
+    } elseif (addQuestion($_SESSION['user']['id'], htmlspecialchars($question))) {
         header('Location: questions.php');
-        exit;
+        exit();
     }
+    echo $err;
 }
 echo '<br>';
 echo '<form action="questions.php" method="GET">';
