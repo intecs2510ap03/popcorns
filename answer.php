ef13ee7ff40ff3468312990f73d8a15846c7316d
10 <intecs.2510ap.10@gmail.com>
2025-12-09 16:07:02 +0900

    9日のコミット文

--- answer.php
+++ answer.php
@@ -2,63 +2,49 @@
 <?php ob_start(); ?>
 <?php require 'header.php'; ?>
 <?php require 'function.php';?>
+<link rel="stylesheet" href="./style/style.css"	>
 
 <?php
+    // PDO関数
+    $pdo = Connect();
 
-$pdo = Connect();
-
-
-    echo '<p>質問</p>';
-
-    // 名前取得
-    $sql=$pdo->prepare('select * from user where id=?');
-    $sql->execute([1]);
-    foreach($sql as $row) {
-        $user_id = $row['id'];
-        $user_loginId = $row['loginId'];
-        $user_password = $row['password'];
-        $user_name = $row['name'];
-    }
+    // 質問Idを変数に格納
+    $questionId = $_REQUEST['questionId'];
 
     // 質問内容取得
-    $sql=$pdo->prepare('select * from question where userId=?');
-    $sql->execute([$user_id]);
-    foreach($sql as $row) {
-        $question_id = $row['id'];
-        $question_userId = $row['userId'];
-        $question_question = $row['question'];
-        $question_date = $row['date'];
-        $question_delFlg = $row['deleteFlg'];
+    $question = getQuestionById($questionId,$pdo);
+
+    echo '<h1>質問</h1>';
+    
+    foreach ($question as $row) {
+        echo '<p">';
+        // ユーザの名前を取得する
+        $name = getUserName($row['userId'],$pdo);
+        echo $name, '<br>';
+        echo $row['question'], '<br>';
+        echo $row['date'];
+        echo '</p>';
     }
 
-    echo $user_name;
-    echo '<br>';
-    echo $question_question;
-    echo '<br>';
-    echo $question_date;
-    echo '<br>';
+    echo '回答';
 
-
-
-echo '回答';
-
-echo '<form action="answer.php" method="POST">';
-echo '<input type="text" name="answer">';
-echo '<input type="hidden" name="question_id" value="1">';
-echo '<input type="submit" value="登録">';
-
+    echo '<form action="answer.php" method="POST">';
+    echo '<input type="text" name="answer">';
+    echo '<input type="hidden" name="questionId" value="', $questionId ,'">';
+    echo '<input type="submit" value="登録">';
+    echo '</form>';
 
 if (isset($_POST['answer'])) {
     $answer = $_POST['answer'];
     $userId = $_SESSION['user']['id'];
-    $questionId = (int)$_POST['question_id']; // 空白除去_
+    $questionId = (int)$_POST['questionId']; 
     $answer = mb_convert_kana($answer, 's'); // 全角スペースを半角に変換
     if (trim($answer) === "") {
         echo '回答を入力してください';
     } elseif (mb_strlen($answer) > 250) {
         echo '回答は250文字以内で入力してください';
     } elseif (addAnswer($questionId,$userId,$answer)) {
-    header('Location: detail.php?question_id=' . $questionId); 
+        header('Location: detail.php?questionId=' . $questionId); 
     exit;     
 } else {
     echo '回答の登録に失敗しました';
