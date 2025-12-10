829e57fb99c57ecfa29ee45ca7de4d86c28bb290
10 <intecs.2510ap.10@gmail.com>
2025-12-10 16:59:19 +0900

    １０日のコミット、クラス名追加しました。

--- answer.php
+++ answer.php
@@ -4,6 +4,8 @@
 <?php require 'function.php';?>
 <link rel="stylesheet" href="./style/style.css"	>
 
+
+<div class="main">
 <?php
     // PDO関数
     $pdo = Connect();
@@ -13,25 +15,35 @@
 
     // 質問内容取得
     $question = getQuestionById($questionId,$pdo);
-
+?>
+    
+ <div class="question-section">
+    
+    <?php
     echo '<h1>質問</h1>';
     
     foreach ($question as $row) {
-        echo '<p">';
+        echo '<div class="questionbox">';
         // ユーザの名前を取得する
         $name = getUserName($row['userId'],$pdo);
-        echo $name, '<br>';
+        echo '<div class="name">',$name, '<br></div>';
         echo $row['question'], '<br>';
-        echo $row['date'];
-        echo '</p>';
+        echo '<div class="date">',$row['date'],'</div>';
+        echo '</div>';
     }
-
-    echo '回答';
+    ?>
+</div>
+   
+    <div class="answer-section">
+<?php
+    echo '<h2>回答</h2>';
 
     echo '<form action="answer.php" method="POST">';
-    echo '<input type="text" name="answer">';
+    echo '<textarea name="answer" cols="70" rows="4" div class="anstexbox">';
+    echo '</textarea>';
+    echo '<br>';
     echo '<input type="hidden" name="questionId" value="', $questionId ,'">';
-    echo '<input type="submit" value="登録">';
+    echo '<input type="submit" value="登録" class="touroku-btn">';
     echo '</form>';
 
 if (isset($_POST['answer'])) {
@@ -51,9 +63,14 @@ if (isset($_POST['answer'])) {
     }}
 
 ?>
+</div>
 
 <!-- 戻るボタン -->
-<form action="detail.php" method="get">
-<input type="submit" value="戻る">
 
+<form action="detail.php" method="get">
+
+<input type="submit" value="戻る" class="btn">
+</form>
+
+</div>
 <?php require 'footer.php'; ?>
