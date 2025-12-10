f7dbe74f35ed3ec3790bec2009c605690e54ab98
mizuki8 <intecs.2510ap.06@gmail.com>
2025-12-10 09:47:30 +0900

    userAdd.php 追加

--- userAdd.php
+++ userAdd.php
@@ -2,6 +2,32 @@
 <?php require 'header.php'; ?>
 <?php require 'function.php';?>
 
+<main>
+    <form action="/userAdd.php" method="post">
+    <h2>利用者登録</h2>
+    <table>
+        <tr>
+            <th>表示名</th>
+            <td><input type="text" name="viewName" placeholder=""></td>
+        </tr>
+        <tr>
+            <th>ユーザーID</th>
+            <td><input type="text" name="userId" placeholder=""></td>
+        </tr>
+        <tr>
+            <th>パスワード</th>
+            <td><input type="text" name="pass" placeholder=""></td>
+        </tr>
+        <tr>
+            <td> <p class="submit"><input type="submit" value="登録"></p></td>
+            <td>エラーメッセージ</td>
+        </tr>
+        <tr>
+            <td> <a href="index.php"><input type="submit" value="戻る"></a></td>
+        </tr>
+    </table>
+    </form>
+</main>
 <?php echo 'userAdd.php'; ?>
 以下機能を追加してください<br>
 ・利用者IDは8文字以上10文字以下<br>
