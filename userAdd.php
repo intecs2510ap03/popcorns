353bcde14740c6c6debaf4f31918b71d6ee031fa
mizuki8 <intecs.2510ap.06@gmail.com>
2025-12-10 16:15:42 +0900

    登録画面の作成

--- userAdd.php
+++ userAdd.php
@@ -1,33 +1,64 @@
 <?php session_start(); ?>
 <?php require 'header.php'; ?>
 <?php require 'function.php';?>
+<?php
+    $err="";
+    $loginId=$password=$name='';
+    if(!isset($_SESSION['user'])){
+    echo '<form action="userAdd.php" method="post">';
+    echo '<h1>利用者登録</h1>';
+    echo '<table>';
+    echo '<tr><th>表示名</th>';
+    echo '<td><input type="text" name="viewName">';
+    echo '</td></tr>';
+    echo '<tr><th>ユーザーID</th>';
+    echo '<td><input type="text" name="userId">';
+    echo '</td></tr>';
+    echo '<tr><th>パスワード</th>';
+    echo '<td><input type="text" name="pass">';
+    echo '</td></tr>';
+    echo '<tr><td>';
+    echo '<input type="submit" value="登録">';
+    echo '</td>';
+    } else {
+        echo 'あなたはすでに会員です。';
+    }
+    if (isset($_POST['pass']) && isset($_POST['userId'])) {
+        $password=$_POST['pass'];
+        $userId=$_POST['userId'];
+    
+     if (preg_match('/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{6,10}$/',$password)) {
+            if (preg_match('/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{8,10}$/',$userId)) {
+        $err='成功';
+        } else {
+        $err='利用者IDは8文字以上10文字以下で入力してください。';
+     }
+        } else {
+        $err='利パスワードは6文字以上10文字以下で入力してください。';
+     }
+    }
 
-<main>
-    <form action="/userAdd.php" method="post">
-    <h2>利用者登録</h2>
-    <table>
-        <tr>
-            <th>表示名</th>
-            <td><input type="text" name="viewName" placeholder=""></td>
-        </tr>
-        <tr>
-            <th>ユーザーID</th>
-            <td><input type="text" name="userId" placeholder=""></td>
-        </tr>
-        <tr>
-            <th>パスワード</th>
-            <td><input type="text" name="pass" placeholder=""></td>
-        </tr>
-        <tr>
-            <td> <p class="submit"><input type="submit" value="登録"></p></td>
-            <td>エラーメッセージ</td>
+            echo '<td>', $err ,'</td>';
+?>
         </tr>
         <tr>
             <td> <a href="index.php"><input type="submit" value="戻る"></a></td>
         </tr>
     </table>
     </form>
-</main>
+
+    <!-- if (isset($_POST['userId'])) {
+    $question = mb_convert_kana($_POST['question'], "s");
+    if (trim($question)==="") {
+        $err = '質問を入力してください';
+    } elseif (mb_strlen($question) > 250) {
+        $err = '質問は250文字以内で入力してください';
+    } elseif (addQuestion($_SESSION['user']['id'], htmlspecialchars($question))) {
+        header('Location: questions.php');
+        exit();
+    }
+    echo $err; -->
+
 <?php echo 'userAdd.php'; ?>
 以下機能を追加してください<br>
 ・利用者IDは8文字以上10文字以下<br>
