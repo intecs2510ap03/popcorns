904c67b841f9db553033087b7295b34029d75d9f
user03 <intecs.2510ap.03@gmail.com>
2025-12-10 12:00:38 +0900

    詳細画面更新

--- detail.php
+++ detail.php
@@ -7,57 +7,80 @@
     $pdo = Connect();
 
     // 質問Idを変数に格納
-    $questionId = $_GET['questionId'];
+    $questionId = $_REQUEST['questionId'];
     
     // 質問タイトル
     echo '<p>質問</p>';
 
     // 質問内容取得
-    $question = getQuestionById($questionId,$pdo);    
+    $question = getQuestionById($questionId,$pdo);  
     
-    foreach ($question as $row) {
-        echo '<p">';
-        // ユーザの名前を取得する
-        $name = getUserName($row['userId'],$pdo);
-        echo $name, '<br>';
-        echo $row['question'], '<br>';
-        echo $row['date'];
-    }
-
-    // 回答ボタン
-    echo '<form action="answer.php" method="get">';
-    echo '<input type="hidden" name="questionId" value="', $questionId ,'">';
-    echo '<input type="submit" value="回答">';
-    echo '</form>';
-    echo '</p>';
-    
-    // 回答内容一覧取得
-    $answer_All = getAnswersByQuestionId($questionId,$pdo);
-    
-    if (!empty($answer_All)) {
+    $err = "";
+    // 削除
+    if(isset($_POST['delete']) && $_POST['delete']==="削除") {
         
-        // 回答タイトル
-        echo '<p class="answer-box">回答</p>';
-
-        // 回答内容
-        foreach ($answer_All as $row) {
-            echo '<p class="answer-box">';
+        //回答削除
+        if(deleteAnswer($_POST['answerId'])){
+            $err = "回答を削除しました。";
+        }else{
+            $err = "回答の削除に失敗しました。";
+        }
+        }else{
+            $err = "不正アクセスです。";
+    }
+     
+    // 表示
+    if (isset($question)) {
+        //質問IDが正しく取得できた場合のみ表示する
+        foreach ($question as $row) {
+            echo '<p">';
             // ユーザの名前を取得する
             $name = getUserName($row['userId'],$pdo);
             echo $name, '<br>';
-            echo $row['answer'], '<br>';
+            echo $row['question'], '<br>';
             echo $row['date'];
-
-            // ユーザーの投稿内容なら削除ボタンの表示
-            if (isset($_SESSION['user']) && $_SESSION['user']['id'] == $row['userId']) {
-                echo '<form action="delete.php" method="post">';
-                echo '<input type="hidden" name="qunestioId" value="', $row['id'] ,'">';
-                echo '<input type="submit" value="削除">';
-                echo '</form>';
-            }
-            
-            echo '</p>';
         }
+
+        // 回答ボタン
+        echo '<form action="answer.php" method="get">';
+        echo '<input type="hidden" name="questionId" value="', $questionId ,'">';
+        echo '<input type="submit" value="回答">';
+        echo '</form>';
+        echo '</p>';
+        
+        // 回答内容一覧取得
+        $answer_All = getAnswersByQuestionId($questionId,$pdo);
+        
+        if (!empty($answer_All)) {
+            
+            // 回答タイトル
+            echo '<p class="answer-box">回答</p>';
+
+            // 回答内容
+            foreach ($answer_All as $row) {
+
+                if ($row['deleteFlg'] == 0) {
+                echo '<p class="answer-box">';
+                // ユーザの名前を取得する
+                $name = getUserName($row['userId'],$pdo);
+                echo $name, '<br>';
+                echo $row['answer'], '<br>';
+                echo $row['date'];
+
+                // ユーザーの投稿内容なら削除ボタンの表示
+                if (isset($_SESSION['user']) && $_SESSION['user']['id'] == $row['userId']) {
+                    echo '<form action="detail.php?questionId=', $questionId ,'" method="post">';
+
+                    echo '<input type="hidden" name="answerId" value="', $row['id'] ,'">';
+
+                    echo '<input type="submit" name="delete" value="削除">';
+                    echo '</form>';
+                }
+                echo '</p>';
+                }            
+            }
+        }
+
     }
 
 ?>
