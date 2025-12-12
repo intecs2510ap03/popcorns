d654175e107038e6241d89b23866b321e6cb7235
mizuki8 <intecs.2510ap.06@gmail.com>
2025-12-12 13:51:59 +0900

    登録画面作成中

--- userAdd.php
+++ userAdd.php
@@ -1,80 +1,75 @@
-userAdd.php<?php session_start(); ?>
+<?php session_start(); ?> <!-- /*sessionでデータを繋げてる* -->
 <?php require 'header.php'; ?>
 <?php require 'function.php';?>
 <?php
-    $loginId=$password=$name=$err="";
-
-    if(!isset($_SESSION['user'])){
-        // $sql=$pdo->prepare('insert into user values(null,?,?,?)');
-        // $sql->execute([$_POST['name'],$_POST['loginId'],$_POST['password']]);
-        // $name=$_POST['name'];
-    echo '<form action="userAdd.php" method="post">';
-    echo '<h1>利用者登録</h1>';
-    echo '<table>';
-    echo '<tr><th>表示名</th>';
-    echo '<td><input type="text" name="viewName">';
-    echo '</td></tr>';
-    echo '<tr><th>ユーザーID</th>';
-    echo '<td><input type="text" name="userId">';
-    echo '</td></tr>';
-    echo '<tr><th>パスワード</th>';
-    echo '<td><input type="text" name="pass">';
-    echo '</td></tr>';
-    echo '<tr><td>';
-    echo '<input type="submit" value="登録">';
-    echo '</td>';
-    } else {
+    // 空にしてる
+    $loginId=$password=$name=$err=""; 
+    // もしユーザーが空なら登録（ログインしてない）、ユーザーが有なら会員です。　
+    if(isset($_SESSION['user'])){//
         echo 'あなたはすでに会員です。';
+    } else {
+        echo '<form action="userAdd.php" method="post">';
+        echo '<h1>利用者登録</h1>';
+        echo '<table>';
+        echo '<tr><th>表示名</th>';
+        echo '<td><input type="text" name="viewName" required maxlength="10">';
+        echo '</td></tr>';
+        echo '<tr><th>ユーザーID</th>';
+        echo '<td><input type="text" name="userId" required maxlength="10">';
+        echo '</td></tr>';
+        echo '<tr><th>パスワード</th>';
+        echo '<td><input type="text" name="pass" required maxlength="10">';
+        echo '</td></tr>';
+        echo '<tr><td>';
+        echo '<input type="submit" value="登録">';
+        echo '</td>';
     }
-    if (isset($_POST['pass']) && isset($_POST['userId'])) {
-        $password=$_POST['pass'];
-        $userId=$_POST['userId'];
+    // 入力された内容　パス　かつ　ID　が入力されていたら　TR
+    if (isset($_POST['pass']) && isset($_POST['userId']) && isset($_POST['viewName'])) {
+        $password=htmlspecialchars($_POST['pass']);
+        $userId=htmlspecialchars($_POST['userId']);
+        $viewName=htmlspecialchars($_POST['viewName']);
+        // 入力ルールに沿っているかパスの次にIDチェック
+
+  //ファンクションキーのisUser関数の処理を行っている
+                // function isUser($userId, $userPw){
+                //     $pdo = Connect();
+                //    ユーザID,パスワードの重複チェック
+                //     $sql = $pdo->prepare('SELECT * FROM user WHERE loginId=? or password=?');
+                //     $sql->execute([$userId,$userPw]);
+                //     // 結果判定
+                //     if ($sql->rowCount() > 0) {
+                //         return true; // 重複あり
+                //     }else {
+                //         return false; // 重複なし
+                //     }
+                // 帰ってきた結果が$resultに入って処理が進む
     
-     if (preg_match('/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{6,10}$/',$password)) {
+    if (preg_match_all('/[ぁ-んァ-ヶ亜-熙]{1,10}$/u', $viewName, $matches)) {
+        if (preg_match('/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{6,10}$/',$password)) {
             if (preg_match('/^(?=.*[a-z])[a-z]{8,10}$/',$userId)) {
-            // $err='成功';
-            $result = isUser($userId,$password);
-            if ($result) {
-                $err= 'このユーザーIDまたはパスワードは使用できません';
+                $result = isUser($userId,$password);
+                if ($result) {
+                    $err= 'このユーザーIDまたはパスワードは使用できません';
+                } else {
+                    $err= '登録できるよ';
+                    addUser($userId,$password,$viewName);
+                    header('Location: index.php');
+                }
             } else {
-                $err= '登録できるよ';
+            $err='ユーザーIDは8文字以上10文字以下で入力してください。';
             }
         } else {
-        $err='ユーザーIDは8文字以上10文字以下で入力してください。';
-     }
-        } else {
         $err='利用パスワードは6文字以上10文字以下で入力してください。';
-     }
-    }
-
+        }
+    }else{
+    $err='表示名は10文字以下の日本語で入力してください。';
+    }}
             echo '<td>', $err ,'</td>';
 ?>
         </tr>
-        <tr>
-            <td> <a href="index.php"><input type="submit" value="戻る"></a></td>
+        <tr><td><a href="index.php"><input type="submit" value="戻る"></a></td>
         </tr>
     </table>
     </form>
-
-    <!-- if (isset($_POST['userId'])) {
-    $question = mb_convert_kana($_POST['question'], "s");
-    if (trim($question)==="") {
-        $err = '質問を入力してください';
-    } elseif (mb_strlen($question) > 250) {
-        $err = '質問は250文字以内で入力してください';
-    } elseif (addQuestion($_SESSION['user']['id'], htmlspecialchars($question))) {
-        header('Location: questions.php');
-        exit();
-    }
-    echo $err; -->
-
-<?php echo 'userAdd.php'; ?>
-以下機能を追加してください<br>
-・利用者IDは8文字以上10文字以下<br>
-・パスワードは6文字以上10文字以下<br>
-・利用者IDとパスワードは半角英数字のみ使用<br>
-・重複した利用者IDは登録不可<br>
-・重複したパスワードも登録不可<br>
-・htmlspecialchars()関数を使用する<br>
-
-<?php require 'footer.php'; ?>
+<?php require 'footer.php';?>
